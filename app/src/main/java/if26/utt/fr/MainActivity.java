f64ba4f763483093c6b1f55a63945c7627c734a2
ranfang19 <ranfang19@gmail.com>
2019-10-01 11:05:04 +0200

    Initial commit

---
+++ app/src/main/java/if26/utt/fr/MainActivity.java
@@ -0,0 +1,29 @@
+package if26.utt.fr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView tx = (TextView) findViewById (R.id.textView2);
+        tx.setText("Message modifié dans le code Java");
+        String message = "Bonjour  IF26";
+        tx.setText(message);
+        message=this.getResources().getString(R.string.message);
+        tx.setText(message);
+
+        Button bt = (Button) findViewById(R.id.myButton); bt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { tx.setBackgroundColor(Color.RED);
+            } });
+
+    }
+}
